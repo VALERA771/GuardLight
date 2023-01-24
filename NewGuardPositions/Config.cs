69a7c87cd445865e81f04d8a787dd609fe4b8f65
VALERA771 <72030575+VALERA771@users.noreply.github.com>
2023-01-24 22:44:09 +0300

    Add files via upload

--- NewGuardPositions/Config.cs
+++ NewGuardPositions/Config.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Exiled.API.Interfaces;
+using MapGeneration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,26 @@ using System.Threading.Tasks;
 
 namespace NewGuardPositions
 {
-    internal class Config
+    public sealed class Config : IConfig
     {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+
+        public uint GuardToTp { get; set; } = 5;
+
+        public List<RoomName> RoomsToTp { get; set; } = new List<RoomName>()
+        {
+            RoomName.LczAirlock,
+            RoomName.LczToilets,
+            RoomName.LczCheckpointA,
+            RoomName.LczCheckpointB,
+            RoomName.LczGlassroom,
+            RoomName.LczClassDSpawn,
+            RoomName.LczArmory,
+            RoomName.Lcz173,
+            RoomName.LczGreenhouse,
+            RoomName.LczComputerRoom
+        };
+
     }
 }
--- NewGuardPositions/Plugin.cs
+++ NewGuardPositions/Plugin.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Exiled.API.Features;
+using MapGeneration;
+using MEC;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,60 @@ using System.Threading.Tasks;
 
 namespace NewGuardPositions
 {
-    public class Class1
+    public class Plugin : Plugin<Config>
     {
+        internal Plugin Instance => _singleton;
+        private Plugin _singleton;
+
+        public override string Name => base.Name;
+        public override string Prefix => base.Prefix;
+        public override string Author => base.Author;
+        public override Version Version => base.Version;
+        public override Version RequiredExiledVersion => base.RequiredExiledVersion;
+
+
+        public override void OnEnabled()
+        {
+            _singleton = this;
+            if (Instance.Config.RoomsToTp.Count == 0)
+            {
+                Instance.Config.RoomsToTp = new List<MapGeneration.RoomName>()
+                {
+                    RoomName.LczAirlock,
+                    RoomName.LczToilets,
+                    RoomName.LczCheckpointA,
+                    RoomName.LczCheckpointB,
+                    RoomName.LczGlassroom,
+                };
+            }
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            _singleton = null;
+            base.OnDisabled();
+        }
+
+        public override void OnReloaded()
+        {
+            base.OnReloaded();
+        }
+
+        
+        public void OnRoundStart()
+        {
+            Timing.CallDelayed(0.5f, () =>
+            {
+                List<Player> guards = Player.Get(PlayerRoles.RoleTypeId.FacilityGuard).ToList();
+                for (int i = 0; i < Instance.Config.GuardToTp; i++)
+                {
+                    Player guard = guards[i];
+                    RoomName name = Instance.Config.RoomsToTp.RandomItem();
+                    Room room = Room.List.Where(x => x.RoomName == name).First();
+                    guard.Teleport(room.Position);
+                }
+            });
+        }
     }
 }
